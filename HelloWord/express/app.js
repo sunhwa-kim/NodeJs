4e1d17ee5ba9d18f1f563183371762f8f194c507
sunhwa-kim <ksh0810@gmail.com>
2020-09-06 15:54:17 +0900

    view engine install - numjunks

--- HelloWord/express/app.js
+++ HelloWord/express/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const admin = require('./routes/admin');   //  admin 파일 추가
+const nunjucks = require('nunjucks');   // view engine (html)
 
 const app = express();
 const port = 3000;   // index.js에서 썼던것의 express 버전
+
+nunjucks.configure('template', {   // 폴더명
+    autoescape: true,   //  보안상
+    express: app   //  express() 객체로
+});
 
 
 app.get('/',(req,res) => {
