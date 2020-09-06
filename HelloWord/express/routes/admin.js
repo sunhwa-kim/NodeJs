4de41c2bda514ce5f76b0a1091dd63917907f075
sunhwa-kim <ksh0810@gmail.com>
2020-09-06 17:24:46 +0900

    extend template and layout, bootstrap,head, body

--- HelloWord/express/routes/admin.js
+++ HelloWord/express/routes/admin.js
@@ -10,7 +10,8 @@ router.get('/', (req,res) => {
 router.get('/products', (req,res) => {
     // res.send('admin 이후 url');
     res.render( 'admin/products.html' ,    //  template 이후의 폴더로 적기
-        { message : "hello !!!!" } // message 란 변수를 템플릿으로 내보낸다.
+        {message : '<h1>hello</h1>'}  //  {{ message | safe}} safe 추가로 태그명 출력 없어지는
+        // { message : "hello !!!!" } // message 란 변수를 템플릿으로 내보낸다.
     );
 });
 
