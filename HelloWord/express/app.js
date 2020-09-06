cbd68543ba86e3cea4d0efc73315df41707a8d47
sunhwa-kim <ksh0810@gmail.com>
2020-09-06 18:45:56 +0900

    middleware - morgan & app.use(), next()

--- HelloWord/express/app.js
+++ HelloWord/express/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const admin = require('./routes/admin');   //  admin 파일 추가
 const nunjucks = require('nunjucks');   // view engine (html)
+const logger = require('morgan');     //  미들웨어 - 로깅정보
 
 const app = express();
 const port = 3000;   // index.js에서 썼던것의 express 버전
@@ -10,10 +11,23 @@ nunjucks.configure('template', {   // 폴더명
     express: app   //  express() 객체로
 });
 
+// 위 -> 아래 순차적 진행 ( 미들웨어, 다른 폴더 화면 등)
+// 미들웨어 셋팅 : app.use()
+app.use(logger('dev'));   // 터미널에 로깅 확인
+
 
 app.get('/',(req,res) => {
     res.send('Hello Express');
 });
+
+/*
+// 테스트용 admin.js의 testMiddleware1,2 와 연결성으로 순서 이해
+function vipMiddleware(req,res, next){
+    console.log('최우선 미들웨어 테스트');
+    next();
+}
+app.use('/admin',vipMiddleware, admin);
+*/
 
 // Routing, 미들웨어 = app.use( 'url'은 파일명 참고해라~)
 app.use('/admin', admin);
--- HelloWord/express/routes/admin.js
+++ HelloWord/express/routes/admin.js
@@ -1,9 +1,36 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req,res) => {
+// router.get('/', (req,res) => {
+//     res.send('admin 이후 url');
+// });
+
+function testMiddleWare( req, res, next ){   // 인자 3개
+    console.log('첫번째 미들웨어');   // 터미널에 뜨는 정보
+    next();   // 제어권 넘긴다
+}
+
+function testMiddleWare2( req, res, next ){
+    console.log('두번째 미들웨어');
+    next();
+}
+router.get('/', testMiddleWare, testMiddleWare2 , (req,res) => {  // /admin 페이지
+    res.send('admin url 이후 -  미들웨어 테스트( morgan - 로깅정보 ) - next()로 제어권 넘겨받은 이후 ');	    // 화면
+});
+/*
+//  미들웨어 실제 사용 예시
+function loginRequired(req,res,next) {
+    if ('로그인 되어 있지 않으면'){
+        res.redirect('로그인 창');
+    } else {
+        next();
+    }
+}
+//  loginRequired 을 분기점으로 로그인 창 또는 요청 창으로 이동하게
+router.get('/',loginRequired, (req,res) => {
     res.send('admin 이후 url');
 });
+*/
 
 
 // view engine 출력 pacitce
