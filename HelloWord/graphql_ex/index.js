fee29b1f5fc95cdd7cda23ff44fc93a1d16ca78a
sunhwa-kim <ksh0810@gmail.com>
2020-09-23 01:57:18 +0900

    graphql crud test with postman

--- HelloWord/graphql_ex/index.js
+++ HelloWord/graphql_ex/index.js
@@ -13,6 +13,9 @@ const { buildSchema } = require('graphql');
 // `);
 
 //  ! : 필수 의미 
+// input 구조
+// type 데이터 구조
+// Query or Mutation (변수명 : 받는 인자) : return 값
 const schema = buildSchema (`
 
     input ProductInput {
@@ -34,6 +37,8 @@ const schema = buildSchema (`
 
     type Mutation {
         addProduct( input : ProductInput ) : Product
+        updateProduct( id:ID! , input : ProductInput! ) : Product
+        deleteProduct( id : ID! ) : String
     }
 `);
 
@@ -56,12 +61,26 @@ const products = [{
     description : 'testtesttest'
 }]
 
+// schema 구체적인 내용
 const root = {
     getProduct : ({id}) => products.find( product => product.id === parseInt(id)) ,
     addProduct : ({input}) => {
         input.id = parseInt(products.length +1);
         products.push(input);
         return root.getProduct({id : input.id})
+    },
+    updateProduct :({id,input}) => {
+        const index = products.findIndex( product => product.id === parseInt(id))
+        products[index] = {
+            id : parseInt(id),
+            ...input
+        }
+        return products[index];
+    },
+    deleteProduct : ({ id }) => {
+        const index = products.findIndex( product => product.id === parseInt(id))
+        products.splice(index, 1)
+        return "removed";
     }
 }
 //위 // return products.find( product => product.id === parseInt(id))  // 인데 root도 접근해서 return
@@ -70,7 +89,12 @@ const root = {
 //     "query": "mutation addProduct($input: ProductInput) { addProduct(input: $input) { id } }",
 //     "variables": { "input" : { "name" : "세번째상품" , "price" : 3000 , "description" : "test" } }
 // }
-
+// update test
+// {
+//     "query": "mutation updateProduct( $id : ID! , $input: ProductInput! ) { updateProduct( id : $id  , input: $input) { id name description } }",
+//     "variables": { "id" : 1 ,"input" : { "name" : "sunhwa 수정 test" , "price" : 1000 , "description" : "id 있는지 graphiql 확인 후 수정 테스트" } }
+// }
+// delete test
 
 
 // graphiql  GUI 환경에서 query 할 수 있게
